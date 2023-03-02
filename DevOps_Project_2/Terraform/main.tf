a258bd6f3d399219cc93c687577f0ba59d602666
Kamal-Basha <106060266+Kamal-Basha@users.noreply.github.com>
2023-03-02 16:54:50 +0530

    Update main.tf

--- DevOps_Project_2/Terraform/main.tf
+++ DevOps_Project_2/Terraform/main.tf
@@ -6,6 +6,11 @@ terraform {
     }
   }
 }
+provider "aws" {
+  region     = "ap-northeast-1"
+  access_key = "AKIAU6CVY2DNYB6BQ6O4"
+  secret_key = "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"
+}
 
 resource "aws_instance" "web1" {
   ami           = "ami-0ffac3e16de16665e"
